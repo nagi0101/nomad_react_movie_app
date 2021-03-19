23e79ea9783d31acdd422a7e096f8ddfbb5c425d
Nagi <demon0315@naver.com>
2021-03-20 00:54:23 +0900

    #6.2 Building the Navigation

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span>About this page : I built it because I love movie</span>;
+  return (
+    <div className="about-container">
+      <span className="about">
+        About this page : I built it because I love movie
+      </span>
+    </div>
+  );
 }
 
 export default About;
