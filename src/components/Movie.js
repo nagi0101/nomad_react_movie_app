8dbdb9809f726fd00e6d514022b81b13322af882
nagiNotebook <demon0315@naver.com>
2021-03-20 16:16:23 +0900

    #6.3 Sharing Props Between Routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,30 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres, index }) {
   return (
-    <div
-      className="movie"
-      data-aos={index > 3 ? "fade-up" : ""}
-      data-aos-anchor-placement={
-        index > 3 && index % 2 === 0 ? "center-bottom" : "bottom-bottom"
-      }
-    >
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre, index) => (
-            <li className="genre" key={index}>
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+    <Link to={{
+      pathname:"/movie-detail",
+      state:{ year, title, summary, poster, genres, index },
+    }}>
+      <div
+        className="movie"
+        data-aos={index > 3 ? "fade-up" : ""}
+        data-aos-anchor-placement={
+          index > 3 && index % 2 === 0 ? "center-bottom" : "bottom-bottom"
+        }
+        >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              <li className="genre" key={index}>
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 Movie.propTypes = {
